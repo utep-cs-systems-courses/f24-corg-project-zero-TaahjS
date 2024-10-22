67d091908e5247932b5a507e5bda7d00666b6aec
TaahjS <79266761+TaahjS@users.noreply.github.com>
2024-10-22 12:07:23 -0600

    Update draw_chars.c

--- draw_chars.c
+++ draw_chars.c
@@ -30,15 +30,18 @@ void print_char_8x12(char c){
 }
 
 //11x16 comes flipped so we have to flip it
-void print_char_11x16(char c){
+void print_char_11x16(char c)
+{
   c -= 0x20;
-  for (char row = 0; row < 11; row++){
-    for (char col = 0; col < 16; col++){
-      unsigned short colBits = font_11x16[c][row];
-      unsigned short rowMask = 1 << (15-col);
-      putchar((colBits & rowMask) ? '*' : ' ');
+  for (char row = 0; row < 11; row++) {
+    unsigned short rowBits = font_11x16[c][row];
+    for (char col = 0; col < 16; col++) {
+      unsigned short colMask = 1 << (15-col);
+      if(rowBits * colMask){
+        putchar('*');
+      }else{
+        putchar(' ');
     }
     putchar('\n');
   }
-  putchar('\n');
 }
